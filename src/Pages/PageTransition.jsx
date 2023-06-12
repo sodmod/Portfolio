756123f0efac7fdc236ae3779f394d8bb526db05
sodmod <horluwatosin1999@gmail.com>
2023-06-12 21:54:41 +0100

    okay

--- src/Pages/PageTransition.jsx
+++ src/Pages/PageTransition.jsx
@@ -1,32 +1,32 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-const opacityAnimation = {
-  initial: {
-    opacity: 0,
-    scale: 0,
-  },
-  animate: {
-    opacity: 1,
-    scale: 1,
-  },
-  exit: {
-    opacity: 0,
-    scale: 0,
-  },
-};
+// const opacityAnimation = {
+//   initial: {
+//     opacity: 0,
+//     scale: 0,
+//   },
+//   animate: {
+//     opacity: 1,
+//     scale: 1,
+//   },
+//   exit: {
+//     opacity: 0,
+//     scale: 0,
+//   },
+// };
 
-const rotateAnimation = {
-  initial: {
-    rotateY: 90,
-  },
-  animate: {
-    rotateY: 0,
-  },
-  exit: {
-    rotateY: -90,
-  },
-};
+// const rotateAnimation = {
+//   initial: {
+//     rotateY: 90,
+//   },
+//   animate: {
+//     rotateY: 0,
+//   },
+//   exit: {
+//     rotateY: -90,
+//   },
+// };
 
 const zoomAnimation = {
   initial: {
@@ -43,18 +43,19 @@ const zoomAnimation = {
   },
 };
 
-const widthAnimation = {
-  initial: {
-    width: "30%",
-  },
-  animate: {
-    width: "100%",
-  },
-  exit: {
-    width: "100%",
-    x: window.innerWidth,
-  },
-};
+// const widthAnimation = {
+//   initial: {
+//     width: "30%",
+//   },
+//   animate: {
+//     width: "100%",
+//   },
+//   exit: {
+//     width: "100%",
+//     x: window.innerWidth,
+//   },
+// };
+
 function PageTransition({ children, className }) {
   return (
     <motion.section
