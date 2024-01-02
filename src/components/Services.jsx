acfc3d7584b05c660328a13db938cad269ac8fe7
sodmod <horluwatosin1999@gmail.com>
2024-01-02 21:48:48 +0100

    service success

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -42,38 +42,6 @@ function Services() {
                   />
                 </Link>
               </div>
-              <div className="service-work-project">
-                <Link to="/" className="service-work-eachproject">
-                  <img
-                    src="https://images.unsplash.com/photo-1703876766452-f26b795a4b2e?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    alt=""
-                  />
-                </Link>
-              </div>
-              <div className="service-work-project">
-                <Link to="/" className="service-work-eachproject">
-                  <img
-                    src="https://images.unsplash.com/photo-1703876766452-f26b795a4b2e?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    alt=""
-                  />
-                </Link>
-              </div>
-              <div className="service-work-project">
-                <Link to="/" className="service-work-eachproject">
-                  <img
-                    src="https://images.unsplash.com/photo-1703876766452-f26b795a4b2e?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    alt=""
-                  />
-                </Link>
-              </div>
-              <div className="service-work-project">
-                <Link to="/" className="service-work-eachproject">
-                  <img
-                    src="https://images.unsplash.com/photo-1703876766452-f26b795a4b2e?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    alt=""
-                  />
-                </Link>
-              </div>
             </div>
           </div>
         </div>
